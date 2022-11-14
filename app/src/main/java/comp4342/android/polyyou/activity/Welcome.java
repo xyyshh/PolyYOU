ef937532f29c96603cf2f81e2045a75b1048bc66
xyyshh <63178184+xyyshh@users.noreply.github.com>
2022-11-14 19:46:37 +0800

    Add some functions in Welcome.

--- app/src/main/java/comp4342/android/polyyou/activity/Welcome.java
+++ app/src/main/java/comp4342/android/polyyou/activity/Welcome.java
@@ -2,15 +2,55 @@ package comp4342.android.polyyou.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import comp4342.android.polyyou.R;
 
 public class Welcome extends AppCompatActivity {
 
+    private Button btnLogin;
+    private Button btnSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        initView();
+        initEvent();
+    }
+
+    private void initEvent() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toLoginActivity();
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toSignUpActivity();
+            }
+        });
+    }
+
+    private void toLoginActivity() {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void toSignUpActivity() {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    private void initView() {
+        btnLogin = findViewById(R.id.button_login);
+        btnSignUp = findViewById(R.id.button_sign_up);
     }
 }
