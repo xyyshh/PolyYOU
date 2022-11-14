a821d4f4550682c7c3113162d394b9a8a3e8d3bc
jehanyang <jehanwakaa@gmail.com>
2022-11-14 19:15:09 +0800

    Notification

---
+++ app/src/main/java/comp4342/android/polyyou/activity/Login.java
@@ -0,0 +1,16 @@
+package comp4342.android.polyyou.activity;
+
+        import androidx.annotation.NonNull;
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import comp4342.android.polyyou.R;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.MenuItem;
+
+        import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Login extends AppCompatActivity {
+
+}
