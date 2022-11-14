19ab0dcb57da50e710fca4292a33e3ad1ad964bb
xyyshh <63178184+xyyshh@users.noreply.github.com>
2022-11-14 17:04:07 +0800

    Rename login to welcome

--- app/src/main/java/comp4342/android/polyyou/activity/Welcome.java
+++ app/src/main/java/comp4342/android/polyyou/activity/Welcome.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import comp4342.android.polyyou.R;
 
-public class Login extends AppCompatActivity {
+public class Welcome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_welcome);
     }
 }
